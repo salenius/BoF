32e0103a525c174304acb6344849528d80b605a3
Tommi Salenius <tommisalenius@gmail.com>
2018-05-21 13:18:14 +0300

    Etätyökansio kytketty

---
+++ markkinaseuranta_adhoc.r
@@ -0,0 +1,5 @@
+summa_yhteen <- function(x,y){
+		return(x+y)  
+}
+
+print(summa_yhteen(2,4))
---
+++ stderror.r
@@ -0,0 +1,9 @@
+#!usr/bin/env Rscript
+####################
+### Author: Tommi Salenius
+### Email: tommisalenius@gmail.com
+### Created: Ma 21 05 18
+### License: General Public License (2018)
+###################
+
+
