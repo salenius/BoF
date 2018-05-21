78e8ec3724eb08603d7249c12fa3d728fe39841a
Tommi Salenius <tommisalenius@gmail.com>
2018-05-21 13:52:11 +0300

    Tein muutoksen, jossa stderror-R-skriptiä muokattiin Windows-koneen kannalta asianmukaisesti.

--- stderror.r
+++ stderror.r
@@ -6,4 +6,13 @@
 ### License: General Public License (2018)
 ###################
 
+# I use this file to calculate the standard deviation for several time series
 
+std <- function(vec){
+		output = sqrt(var(vec,na.rm=TRUE))
+		return(output)
+}
+
+# Lataa tiedosto
+# setwd("C:\\Users\\saleniusto\\Desktop\\Kehitystyö\\Financial indicators")
+omx <- xlsx::read.xlsx("omxhelsinki.xlsx",sheetName="Taul1")
