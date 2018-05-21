6af4faef837a00ef2de2f775abca32810e825b37
salenius <38977281+salenius@users.noreply.github.com>
2018-05-21 13:57:03 +0300

    Update stderror.r
    Laskettu standardipoikkeama

--- stderror.r
+++ stderror.r
@@ -9,10 +9,15 @@
 # I use this file to calculate the standard deviation for several time series
 
 std <- function(vec){
-		output = sqrt(var(vec,na.rm=TRUE))
-		return(output)
+  output = sqrt(var(vec,na.rm=TRUE))
+  return(output)
 }
 
 # Lataa tiedosto
 # setwd("C:\\Users\\saleniusto\\Desktop\\Kehitystyö\\Financial indicators")
 omx <- xlsx::read.xlsx("omxhelsinki.xlsx",sheetName="Taul1")
+omx <- subset(omx,!is.na(omx[,1]))
+
+# Laske OMX Helsingin yleisindeksin std.dev 2013-2017
+print(std(omx[,2]))
+# 1055.507
