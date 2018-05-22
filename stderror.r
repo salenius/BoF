b5bbcdedb63457c19948071075c5fdf375c3b892
Tommi Salenius <tommisalenius@gmail.com>
2018-05-22 12:39:27 +0300

    Dataframet sarakeviittaukset oikaistu

--- stderror.r
+++ stderror.r
@@ -100,8 +100,8 @@ xlsx::write.xlsx(bond10std[,3:ncol(bond10std)],"valtionlainat_kkstdev.xlsx",shee
 eur1$month <- strftime(eur1$date,format="%m")
 eur1$year <- strftime(eur1$date,format="%y")
 
-eur1mean <- aggregate(x=eur1[,2],by=eur1[,8:9],FUN=mean,na.rm=T)
-eur1std <- aggregate(x=eur1[,2],by=eur1[,8:9],FUN=std)
+eur1mean <- aggregate(x=eur1[,2],by=eur1[,3:4],FUN=mean,na.rm=T)
+eur1std <- aggregate(x=eur1[,2],by=eur1[,3:4],FUN=std)
 
 eur1mean$year <- paste("20",eur1mean$year,sep="")
 eur1std$year <- paste("20",eur1std$year,sep="")
@@ -117,8 +117,8 @@ xlsx::write.xlsx(eur1std[,3:ncol(eur1std)],"euribor1_kkstdev.xlsx",sheetName="Kk
 eur3$month <- strftime(eur3$date,format="%m")
 eur3$year <- strftime(eur3$date,format="%y")
 
-eur3mean <- aggregate(x=eur3[,2],by=eur3[,8:9],FUN=mean,na.rm=T)
-eur3std <- aggregate(x=eur3[,2],by=eur3[,8:9],FUN=std)
+eur3mean <- aggregate(x=eur3[,2],by=eur3[,3:4],FUN=mean,na.rm=T)
+eur3std <- aggregate(x=eur3[,2],by=eur3[,3:4],FUN=std)
 
 eur3mean$year <- paste("20",eur3mean$year,sep="")
 eur3std$year <- paste("20",eur3std$year,sep="")
