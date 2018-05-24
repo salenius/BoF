2f42b3f8c491afd35d11adf9453e65d2b61c6737
salenius <38977281+salenius@users.noreply.github.com>
2018-05-24 18:05:02 +0300

    Update metadatareader.R
    Metadatareader toimii

--- metadatareader.R
+++ metadatareader.R
@@ -14,11 +14,39 @@
 
 lue.metadata <- function(sheet) {
 
-		df1 <- xlsx::read.xlsx("metadata.xlsx",sheetName=sheet) 
+  df1 <- xlsx::read.xlsx("metadata.xlsx",sheetName=sheet) 
 
-		return(df1)
+  for(i in df1$Chart){
+    if(!(i %in% c("line","bar","area","yield"))){
+      stop(paste("Tunnistamaton parametri tiedostossa metadata.xlsx välilehdellä ", sheet, ". Tarkista
+                 että kaikki Chart-sarakkeen muuttujat ovat joukossa 'line', 'bar', 'area' tai 'yield'.", sep=""))
+    }
+  }
+  
+  #rownames(df1) = apply(df1, 1, function(x) paste0(x[1], " ", x[2]))
+  
+  df1$Ticker <- as.character(df1$Ticker)
+  df1$Field <- as.character(df1$Field)
+  
+  # Jos yield-parametria on käytetty Chart-sarakkeessa, lisää taulukkoon uusi sarake, jossa
+  # on kyseistä parametria käyttävän assetin maturiteetti. Oikean maturiteetin asettaa yieldcurve.r
+  
+  if("yield" %in% levels(df1$Chart)){
+    df1$Maturity <- numeric(nrow(df1))
+    source("Funktiot/yieldcurve.r")
+    for(i in 1:nrow(df1)){
+      
+      if(df1$Chart[i] == "yield"){ df1$Maturity[i] = maturitycheck(i,df1) }
+      
+      
+    }
+  }
+  
+  return(df1)
 
 }
 
+metad <- lue.metadata("Sheet1")
+metad
 
 
